2b1bea7522bcb3ca100be1d1f35c6a82509744c7
unknown <yetihatgt@gmail.com>
2023-12-11 14:27:26 +0200

    y

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import {useEffect} from "react";
+function App() {
+  useEffect(() => {
+    async function getData() {
+      const response = await fetch("http://localhost:80/");
+      const data = await response.json();
+      console.log(data);
+    }
+    getData();
+  },[]);
+  return <h1>Hi backend</h1>
+  
+}
+
+export default App;
